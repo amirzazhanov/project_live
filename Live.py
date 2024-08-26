c569fda70e91bc9e6011ca0c0888e07b7125e08c
amirzazhanov <adel.mirzazhanov@gmail.com>
2024-08-26 16:01:47 +0500

    World of Games - Level 1

--- Live.py
+++ Live.py
@@ -1,1 +1,27 @@
 
+#
+#  This function has a person name as an input and returns a string in the following layout:
+#
+def welcome(name):
+    return f"Hello {name} and welcome to the World of Games (WoG).\nHere you can find many cool games to play."
+#
+#  This function Gets an input from the user about the game he chose. After receiving the game number from 
+#  the user, the function will get the level of difficulty with the following text and also save to a 
+#  variable:
+#
+def load_game():
+    print ("Please choose a game to play:\n")
+    print ("\t1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back")
+    print ("\t2. Guess Game - guess a number and see if you chose like the computer")
+    print ("\t3. Currency Roulette - try and guess the value of a random amount of USD in ILS")
+    while True:
+            game = int(input("Enter game number (1-3): "))
+            if game >=1 and game <= 3:
+                break
+    
+    while True:
+            level = int(input("Please choose game difficulty level 1-5: "))
+            if level >=1 and level <= 5:
+                break
+   
+    return game, level
